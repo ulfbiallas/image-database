f21a522065d246f463509ce4637d92a744bf7c13
ulfbiallas <ulfbiallas@googlemail.com>
2015-04-25 13:50:14 +0200

    created feature entity and repository

---
+++ src/main/java/de/ulfbiallas/imagedatabase/repository/FeatureRepository.java
@@ -0,0 +1,13 @@
+package de.ulfbiallas.imagedatabase.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import de.ulfbiallas.imagedatabase.entities.Feature;
+
+
+
+@Repository
+public interface FeatureRepository extends JpaRepository<Feature, Long>{
+
+}

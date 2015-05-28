2fbd5dc38e0a418a6f5ed1371e3d52e4833699b0
ulfbiallas <ulfbiallas@googlemail.com>
2015-05-28 23:20:37 +0200

    added cache configuration

--- src/main/java/de/ulfbiallas/imagedatabase/tools/Cache.java
+++ src/main/java/de/ulfbiallas/imagedatabase/tools/Cache.java
@@ -12,14 +12,37 @@ import org.springframework.stereotype.Component;
 @EnableCaching
 public class Cache {
 
+    private final String LEAST_FREQUENTLY_USED = "LFU";
+
+
+
     @Bean
     CacheManager cacheManager() {
-        net.sf.ehcache.CacheManager cacheManager = net.sf.ehcache.CacheManager.getInstance();
+        net.sf.ehcache.config.Configuration ehCacheConfiguration = createEhCacheConfiguration();
+        net.sf.ehcache.CacheManager cacheManager = net.sf.ehcache.CacheManager.newInstance(ehCacheConfiguration);
 
         cacheManager.addCache("imagerecord");
         cacheManager.addCache("tag");
 
         return new EhCacheCacheManager(cacheManager);
+    }
+
+
+
+    private net.sf.ehcache.config.Configuration createEhCacheConfiguration() {
+        net.sf.ehcache.config.Configuration ehCacheConfiguration = new net.sf.ehcache.config.Configuration();
+        ehCacheConfiguration.addDefaultCache(createEhCacheCacheConfiguration());
+        return ehCacheConfiguration;
+    }
+
+
+
+    private net.sf.ehcache.config.CacheConfiguration createEhCacheCacheConfiguration() {
+        net.sf.ehcache.config.CacheConfiguration ehCacheCacheConfiguration = new net.sf.ehcache.config.CacheConfiguration();
+        ehCacheCacheConfiguration.setName("ImageDatabaseCache");
+        ehCacheCacheConfiguration.setMemoryStoreEvictionPolicy(LEAST_FREQUENTLY_USED);
+        ehCacheCacheConfiguration.setMaxEntriesLocalHeap(1000);
+        return ehCacheCacheConfiguration;
     }
 
 }

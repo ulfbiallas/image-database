fa5b1a132be03461306389dc3cd1a7d0274bb59b
ulfbiallas <ulfbiallas@googlemail.com>
2015-05-28 23:06:33 +0200

    replaced SimpleCacheManager with EhCache

--- src/main/java/de/ulfbiallas/imagedatabase/tools/Cache.java
+++ src/main/java/de/ulfbiallas/imagedatabase/tools/Cache.java
@@ -1,11 +1,8 @@
 package de.ulfbiallas.imagedatabase.tools;
 
-import java.util.Arrays;
-
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -17,14 +14,12 @@ public class Cache {
 
     @Bean
     CacheManager cacheManager() {
+        net.sf.ehcache.CacheManager cacheManager = net.sf.ehcache.CacheManager.getInstance();
 
-        SimpleCacheManager cacheManager = new SimpleCacheManager();
-        cacheManager.setCaches(Arrays.asList(
-            new ConcurrentMapCache("imagerecord"),
-            new ConcurrentMapCache("tag")
-        ));
+        cacheManager.addCache("imagerecord");
+        cacheManager.addCache("tag");
 
-        return cacheManager;
+        return new EhCacheCacheManager(cacheManager);
     }
 
 }

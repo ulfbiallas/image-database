587e5ac72c9daad0c52ba557109f05e884c7f780
ulfbiallas <ulfbiallas@googlemail.com>
2015-06-08 15:08:56 +0200

    used system info service for info controller

--- src/main/java/de/ulfbiallas/imagedatabase/controller/InfoController.java
+++ src/main/java/de/ulfbiallas/imagedatabase/controller/InfoController.java
@@ -5,51 +5,27 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-import net.sf.ehcache.Cache;
-import net.sf.ehcache.statistics.StatisticsGateway;
-
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
+import de.ulfbiallas.imagedatabase.service.SystemInfoService;
 
 
 
 @Path("info")
 public class InfoController {
 
-    private final CacheManager cacheManager;
+    private final SystemInfoService systemInfoService;
 
 
 
     @Inject
-    public InfoController(final CacheManager cacheManager) {
-        this.cacheManager = cacheManager;
+    public InfoController(final SystemInfoService systemInfoService) {
+        this.systemInfoService = systemInfoService;
     }
 
 
 
     @GET
     public Response getInfo() {
-        String info = "<h1>Image Database</h1>";
-
-        String osName = System.getProperty("os.name");
-        String osArch = System.getProperty("os.arch");
-        info += "<b>OS</b>: " + osName + " (" + osArch + ")<br>";
-
-        String javaVendor = System.getProperty("java.vendor");
-        String javaVersion = System.getProperty("java.version");
-        info += "<b>Java</b>: " + javaVendor + " (" + javaVersion + ")<br>";
-
-        EhCacheCacheManager ehCacheCacheManager = (EhCacheCacheManager) cacheManager;
-        for (String name : ehCacheCacheManager.getCacheNames()) {
-            info += "<br>";
-            info += "<b>Cache</b>: " + name + "<br>";
-            Cache cache = ehCacheCacheManager.getCacheManager().getCache(name);
-            StatisticsGateway stats = cache.getStatistics();
-            info += "localHeapSizeInBytes: " + stats.getLocalHeapSizeInBytes() + "<br>";
-            info += "cacheHitCount: " + stats.cacheHitCount() + "<br>";
-            info += "cacheMissCount: " + stats.cacheMissCount() + "<br>";
-        }
-
+        String info = systemInfoService.getSystemInfo();
         return Response.ok(info).build();
     }
 

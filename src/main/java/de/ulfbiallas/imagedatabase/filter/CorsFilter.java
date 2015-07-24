f1e53821f6f857ea27574b3ddf35cc8707aea72a
ulfbiallas <ulfbiallas@googlemail.com>
2015-07-24 22:18:23 +0200

    allow Authorization header in cors request

--- src/main/java/de/ulfbiallas/imagedatabase/filter/CorsFilter.java
+++ src/main/java/de/ulfbiallas/imagedatabase/filter/CorsFilter.java
@@ -13,6 +13,7 @@ public class CorsFilter implements ContainerResponseFilter {
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
 		final MultivaluedMap<String,Object> headers = responseContext.getHeaders();
 		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Headers", "Authorization");
 	}
 
 }
